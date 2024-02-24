36925ee439356895d0d1a9b8c5fda0fdae8e6f85
YeaButActuallyNo <srjdatta7@gmail.com>
2024-02-23 21:35:55 -0600

    read/write to files

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 from translate import Translator
 
-translator = Translator(provider="mymemory", to_lang="vi")
+#create the translator object variable something idk what python is
+translator = Translator(to_lang='en', from_lang='bn')
 
-print(translator.translate(""))
+#r is read only
+#w is for rewrite 
+#w+ is for read and write
+#a is for append
+file1 = open('MyFile.dat', 'r', encoding='utf-8')
+
+#close the file
+raw_text = file1.read()
+
+#create the translator
+output = translator.translate(raw_text)
+
+file2 = open('MyFile.txt', 'w')
+
+
+file1.close
+file2.close
